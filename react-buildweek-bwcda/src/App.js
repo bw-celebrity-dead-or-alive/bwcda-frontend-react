39d80e3db92ce96c3e0fd2c9e75ae482987337d9
Kristin Barr <kristinbarr@gmail.com>
2019-07-29 19:40:23 -0400

    App routes setup

--- react-buildweek-bwcda/src/App.js
+++ react-buildweek-bwcda/src/App.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
+import Nav from './Components/Nav'
+import StartScreen from './Components/StartScreen';
+import GameScreen from './Components/GameScreen'
+import EndingScreen from './Components/EndingScreen'
+import Err404 from './Components/Err404'
+import './App.css'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav />
+      <div className='main-content'></div>
+      <Switch>
+        <Route exact path='/' component={StartScreen} />
+        <Route path='/play' component={GameScreen} />
+        <Route path='/end' component={EndingScreen} />
+        <Route component={Err404} />
+      </Switch>
     </div>
-  );
+  )
 }
-
-export default App;
-
-
-// Page that the user sees after they click the play now button on the landing page
-
-
-//Comps
-
-//User can choose rather to log in or just to play
-
-//> click play > goes to game screen
