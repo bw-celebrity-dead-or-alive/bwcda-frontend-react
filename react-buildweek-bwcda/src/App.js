b478221cb93a241c68f4244735bd86c01c8a2f72
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-07-29 16:41:17 -0500

    getting data, picture data isnt being pulled correctly yet

--- react-buildweek-bwcda/src/App.js
+++ react-buildweek-bwcda/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import GameScreen from './Components/GameScreen'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GameScreen />
     </div>
   );
 }
@@ -28,9 +15,9 @@ export default App;
 
 // Page that the user sees after they click the play now button on the landing page
 
-
 //Comps
 
 //User can choose rather to log in or just to play
 
-//> click play > goes to game screen
+// click play > goes to game screen
+
--- react-buildweek-bwcda/src/Components/GameScreen.js
+++ react-buildweek-bwcda/src/Components/GameScreen.js
@@ -8,10 +8,55 @@
 
 //will be pulling data from an api and be pushing that to celebcard.js
 
-//put the use effect in here rather than the cardlist
+
 
 //gamescreen will also house the buttons instead of another comp
 
 //funtion above will handle all the choices that handeles the dead or alive answers that will trigger a visual cue
 
 //score tracker will be also held here, will have its own usestate and when the answer is true the score will have a point added to it. will also display current score
+
+import React, { useEffect, useState } from 'react';
+// import CelebCard from './CelebCard';
+// import Timebar from './Timebar';
+import axios from 'axios'
+
+
+const GameScreen = () => {
+const [data, setData] = useState([])
+const [data1, setData1] = useState([])
+
+        //put the use effect in here rather than the cardlist
+        useEffect(() => {
+            axios.get('https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/')
+            .then(e => {
+                setData(e.data)
+            })
+            .catch(err => {return('Something isnt working', err)})
+    }, [])
+
+            // useEffect(() => {
+            //     axios.get(`https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/picture/${data.map(e=>e.id)}`)
+            //         .then(e => {
+            //             setData1([...data1, e.data])
+            //         })
+            //         .catch(err => {
+            //             return ('Something isnt working', err)
+            //         })
+            // }, [data1])
+
+    return(
+        <div>
+            <div>{console.log('data out of the function' , data.map(e => e))}</div>
+            {/* <div>{console.log('data out of the function' , data1.map(e => e))}</div>*/}
+
+            <div>{/*In here we will have the logo that sits on top on the timer button */}</div>
+            <div> {/* Time Bar will go here and will have a useState that tracks the ending of the time */ } </div>
+            <div>{/* CelebCard */}</div>
+            <div>{/*Buttons for dead or alive*/}</div>
+        </div>
+    )
+
+}
+
+export default GameScreen
