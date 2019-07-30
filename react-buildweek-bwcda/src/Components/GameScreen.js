a2c553f565a458318a9f12ec6ddb66daa225746e
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-30 15:12:10 -0700

    changed the axios call to grab single id celebs to provide endless dead or alive

--- react-buildweek-bwcda/src/Components/GameScreen.js
+++ react-buildweek-bwcda/src/Components/GameScreen.js
@@ -1,3 +1,7 @@
+//Add more names to the data Array
+//Sort actors with no images
+//Figure out the Logic for randomizing the array of data celebs
+
 import React, { useEffect, useState } from "react";
 import CelebCard from './CelebCard';
 import Timebar from './Timebar';
@@ -8,7 +12,7 @@ import logo from '../logo.svg'
 
 const GameScreen = () => {
   //This fetches the list of celebs
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   //This works with SetCurrentCard to identify the current ID needed to be passed
   const [id, setId] = useState(0);
   //Keeps track of Score and resets to zero after game ends.
@@ -19,14 +23,14 @@ const GameScreen = () => {
   //Grabs Data from API
   useEffect(() => {
     axios
-      .get("https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/?page=1,limit=14")
+      .get(`https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/${id}`)
       .then(e => {
         setData(e.data);
       })
       .catch(err => {
         return ("Something isnt working", err);
       });
-  }, []);
+  }, [id]);
 
   //Checks for death case on Click
   const isDead = (deathCheck) => {
@@ -64,13 +68,15 @@ const GameScreen = () => {
             <div>
               <img src={logo} className='App-logo' alt='logo' />
               <h1>Guesses:{id}</h1>
+              <h1>Score:{score}</h1>
+
               <div>{/*In here we will have the logo that sits on top on the timer button */}</div>
               <div>{" "}{/* Time Bar will go here and will have a useState that tracks the ending of the time */}{" "}</div>
 
-              {data[id] ? <CelebCard data={data[id]} /> : <div>Loading...</div>}
+              {data ? <CelebCard data={data} /> : <div>Loading...</div>}
 
-              <button onClick={() => isDead(data[id].death)}>Dead</button>
-              <button onClick={() => isAlive(data[id].death)}>Alive</button>
+              <button onClick={() => isDead(data.death)}>Dead</button>
+              <button onClick={() => isAlive(data.death)}>Alive</button>
 
               <Timebar />
             </div>
