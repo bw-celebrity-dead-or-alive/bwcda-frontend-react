70f8e2f69e85448cd65d6595cd5ea663631e4941
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-07-29 23:35:23 -0500

    timebar base added, slight data manipulation in gamescreen.js

--- react-buildweek-bwcda/src/App.js
+++ react-buildweek-bwcda/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+// import { Switch, Route } from 'react-router-dom'
 
-import Nav from './Components/Nav'
-import StartScreen from './Components/StartScreen';
+// import Nav from './Components/Nav'
+// import StartScreen from './Components/StartScreen';
 import GameScreen from './Components/GameScreen'
-import EndingScreen from './Components/EndingScreen'
-import Err404 from './Components/Err404'
+// import EndingScreen from './Components/EndingScreen'
+// import Err404 from './Components/Err404'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
-      <Nav />
+      <GameScreen />
+      {/* <Nav />
       <div className='main-content'></div>
       <Switch>
         <Route exact path='/' component={StartScreen} />
         <Route path='/play' component={GameScreen} />
         <Route path='/end' component={EndingScreen} />
         <Route component={Err404} />
-      </Switch>
+      </Switch> */}
     </div>
   )
 }
