80c9597d7137df581698e20047f5988f920e7c11
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-07-30 11:17:24 -0500

    added death check

--- react-buildweek-bwcda/src/App.js
+++ react-buildweek-bwcda/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-// import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
-// import Nav from './Components/Nav'
-// import StartScreen from './Components/StartScreen';
+import Nav from './Components/Nav'
+import StartScreen from './Components/StartScreen';
 import GameScreen from './Components/GameScreen'
 import EndingScreen from './Components/EndingScreen'
 import Err404 from './Components/Err404'
@@ -11,15 +11,15 @@ import '../src/scss/App.scss'
 export default function App() {
   return (
     <div className='App'>
-      <GameScreen />
-      {/* <Nav />
+      {/* <GameScreen /> */}
+      <Nav />
       <div className='main-content'></div>
       <Switch>
         <Route exact path='/' component={StartScreen} />
         <Route path='/play' component={GameScreen} />
         <Route path='/end' component={EndingScreen} />
         <Route component={Err404} />
-      </Switch> */}
+      </Switch>
     </div>
   )
 }
--- react-buildweek-bwcda/src/Components/GameScreen.js
+++ react-buildweek-bwcda/src/Components/GameScreen.js
@@ -15,23 +15,23 @@
 //score tracker will be also held here, will have its own usestate and when the answer is true the score will have a point added to it. will also display current score
 
 import React, { useEffect, useState } from "react";
-// import CelebCard from './CelebCard';
+import CelebCard from './CelebCard';
 import Timebar from './Timebar';
 import axios from "axios";
 
 const GameScreen = () => {
   const [data, setData] = useState([]);
+  const [currentCard, setCurrentCard] = useState([]);
   const [id, setId] = useState(0);
-  const [img, setImg] = useState('');
 
   // 1st fetch all the celebs we want 
   // render one after the other to the screen
   // when one is rendered, fetch his image
-
   //put the use effect in here rather than the cardlist
+
   useEffect(() => {
     axios
-      .get("https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/")
+      .get("https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/?page=1,limit=14")
       .then(e => {
         setData(e.data);
       })
@@ -39,24 +39,32 @@ const GameScreen = () => {
         return ("Something isnt working", err);
       });
   }, []);
-  useEffect(() => {
-    axios
-      .get(
-        `https://prod-celebrity-dead-alive.herokuapp.com/api/celebrities/picture/${id})}`
-      )
-      .then(e => {
-        setImg(e.data)
-      })
-      .catch(err => {
-        return ("Something isnt working", err);
-      });
-  }, [id]);
 
+  const SetCurrentCard = (id) => {
+    setCurrentCard(currentCard[id])
+  }
 
+  const isAlive = (aliveCheck) => {
+    if(deathCheck > 0){
+      //If this is true then the person is dead and score is added
+    } else {
+      //If the death date is less than 0 then that person is dead and 
+    }
+  }
+
+   const isDead = (deathCheck) => {
+     if (deathCheck > 0) {
+       //If this is true then the person is dead and score is added
+     } else {
+       //If the death date is less than 0 then that person is dead and 
+     }
+   }
   
   return (
     <div>   
       <div>{console.log("data out of the function", data.map(e => e.name))}</div>
+      {SetCurrentCard(id)}
+      <CelebCard data={currentCard} />
       {/* <div>{console.log(data.filter(e => e))}<div/> */}
 
       <div>{/*In here we will have the logo that sits on top on the timer button */}</div>
@@ -64,9 +72,14 @@ const GameScreen = () => {
       <div>{/* CelebCard */}</div>
       <div>{/*Buttons for dead or alive*/}</div>
 
+      <button onClick={()=> isDead(currentCard.death)}></button>
+      <button onClick={()=> isAlive(currentCard.death)}></button>
+
       <h2>Holding</h2>
         <img src={img} alt="Testing"/>
         <button onClick={() => setId(id + 1)}>+1</button>
+        {/* <button onClick={() => setId(id + 1)}><h2>{data.map(e => e.name[id])}</h2></button> */}
+        {/*button above is printing some crazy stuff.*/} 
       {console.log('data[id]', data[id])}
       <Timebar />
       </div>
