abceb42dd5061146fe4424d38ff788c9a12584ee
Kristin Barr <kristinbarr@gmail.com>
2019-07-30 11:19:07 -0400

    updated button comp

--- react-buildweek-bwcda/src/Components/Buttons.js
+++ react-buildweek-bwcda/src/Components/Buttons.js
@@ -4,24 +4,22 @@ import EndingScreen from './EndingScreen'
 import StartScreen from './StartScreen'
 import Login from './Login'
 
-const Buttons = (buttonText) => {
+const Buttons = (props) => {
+  // button text defaults as 'START PLAYING'
+  let buttonText = 'START PLAYING'
+  let screen = StartScreen
 
-  let button = StartScreen
-  let slug = 'start'
-
-  if (buttonText === 'START PLAYING') {
-    button = StartScreen
-  } else {
-    button = EndingScreen
-    slug = 'end'
+  if (props.origin !== 'START PLAYING') {
+    buttonText = 'PLAY AGAIN'
+    screen = EndingScreen
   }
 
   return (
     <div>
       <button><Link to={Login} >LOGIN</Link></button>
-      <button><Link to={button} >{buttonText}</Link></button>
+      <button><Link to={screen} >{buttonText}</Link></button>
       <Route path='/login' />
-      <Route path={`/${slug}`} />
+      <Route path='/start' />
     </div>
   )
 }
