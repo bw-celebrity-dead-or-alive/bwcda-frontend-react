0e8fd4f5650da31783f7c5d7018eef0bce24bbe7
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-30 12:51:42 -0700

    Getting up to date

--- react-buildweek-bwcda/src/Components/CelebCard.js
+++ react-buildweek-bwcda/src/Components/CelebCard.js
@@ -5,12 +5,10 @@ import React from "react";
 const CelebCard = (props) => {
     return (
         <div>
-            <div>{props.data.id}</div>
+            <img src={props.data.image_url} />
             <div>{props.data.name}</div>
             <div>{props.data.info}</div>
-            <div>{props.data.image_url}</div>
             <div>{props.data.birth}</div>
-            <div>{props.data.death}</div>
         </div>
     )
 }
