ca171ac3e2a71bf00fbefe5528bdb7d80663c196
Kristin Barr <kristinbarr@gmail.com>
2019-07-30 16:18:44 -0400

    added login route

--- react-buildweek-bwcda/src/App.js
+++ react-buildweek-bwcda/src/App.js
@@ -5,6 +5,7 @@ import Nav from './Components/Nav'
 import StartScreen from './Components/StartScreen';
 import GameScreen from './Components/GameScreen'
 import EndingScreen from './Components/EndingScreen'
+import Login from './Components/Login'
 import Err404 from './Components/Err404'
 import '../src/scss/App.scss'
 
@@ -16,6 +17,7 @@ export default function App() {
         <Route exact path='/' component={StartScreen} />
         <Route path='/play' component={GameScreen} />
         <Route path='/end' component={EndingScreen} />
+        <Route path='/login' component={Login} />
         <Route component={Err404} />
       </Switch>
     </div>
