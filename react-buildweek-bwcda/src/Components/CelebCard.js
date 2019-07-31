0bb1272e6a84010abe889348270b8eee50073a4d
Kristin Barr <kristinbarr@gmail.com>
2019-07-31 11:19:58 -0400

    celebcard birthyear h5 to h4

--- react-buildweek-bwcda/src/Components/CelebCard.js
+++ react-buildweek-bwcda/src/Components/CelebCard.js
@@ -1,16 +1,16 @@
 //receives data and styles, mainly a dumb component
 
-import React from "react";
+import React from 'react'
 
 const CelebCard = (props) => {
-    return (
-        <div className='celeb-card'>
-            <img src={props.data.image_url} />
-            <h3>{props.data.name}</h3>
-            <h4>{props.data.info}</h4>
-            <h5>Birth Year: {props.data.birth}</h5>
-        </div>
-    )
+  return (
+    <div className='celeb-card'>
+      <img src={props.data.image_url} />
+      <h3>{props.data.name}</h3>
+      <h4>{props.data.info}</h4>
+      <h4>Birth Year: {props.data.birth}</h4>
+    </div>
+  )
 }
 
-export default CelebCard;
+export default CelebCard
