bcb9a2b12073596ce733c8c6fb0c7dd7a074067b
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-31 14:32:05 -0700

    Added Login Page

--- react-buildweek-bwcda/src/Components/Login.js
+++ react-buildweek-bwcda/src/Components/Login.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from './Button'
+import NameForm from './NameForm'
 
-const LoginScreen =() => {
+
+
+
+
+const LoginScreen = () => {
+
+  const [loginCred, setloginCred] = useState({ username: '', password: '' })
+
+
+  const handleChange = (e) => {
+    setloginCred({ ...loginCred, [e.target.name]: e.target.value })
+  }
+
+
   return (
-    <>
-    </>
+    <div className='end-screen'>
+      <div className='end-content'>
+        <div className='name-form'>
+          <form onSubmit=''>
+            <label htmlFor='username'>
+              Username
+          <input
+                placeholder='username'
+                value={loginCred.username}
+                name='username'
+                onChange={handleChange}
+              />
+            </label>
+            <label htmlFor='password'>
+              Password
+          <input
+                type="password"
+                placeholder='password'
+                value={loginCred.password}
+                name='password'
+                onChange={handleChange}
+              />
+            </label>
+            <Button type='submit' buttonText={'LOGIN'} />
+          </form>
+        </div></div></div>
   )
 }
 
