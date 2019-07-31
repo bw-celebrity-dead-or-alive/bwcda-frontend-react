fc0bbda6d66e4bffb3dc97a15776ef067ff7ac92
Kristin Barr <kristinbarr@gmail.com>
2019-07-31 13:47:33 -0400

    refactored leaderboard to handle live data

--- react-buildweek-bwcda/src/Components/LeaderBoard.js
+++ react-buildweek-bwcda/src/Components/LeaderBoard.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Button from './Button'
 
 const LeaderBoard = () => {
-  const score = window.localStorage.getItem('HighScore')
-  const guess = window.localStorage.getItem('TotalGuesses')
+  const [highScores, setHighScores] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://prod-celebrity-dead-alive.herokuapp.com/api/leaderboard/')
+      .then((res) => {
+        const highScoresArr = res.data
+          .sort((a, b) => {
+            return b.score - a.score
+          })
+          .slice(0, 10)
+        setHighScores(highScoresArr)
+      })
+      .catch((err) => {
+        return 'Failed to load leaderboard', err
+      })
+  }, [])
 
   return (
     <div className='leaderboard-screen'>
@@ -14,38 +30,32 @@ const LeaderBoard = () => {
           <h2 className='name-rank'>NAME</h2>
           <h2 className='score-rank'>SCORE</h2>
         </div>
-        {/* possibly map through data instead of hardcoding */}
         <div className='grid-container'>
-          <h3 className='row1'>1</h3>
-          <h3 className='row1'>name1</h3>
-          <h3 className='row1'>28</h3>
-          <h3 className='row2'>2</h3>
-          <h3 className='row2'>name2</h3>
-          <h3 className='row2'>24</h3>
-          <h3 className='row3'>3</h3>
-          <h3 className='row3'>name3</h3>
-          <h3 className='row3'>23</h3>
-          <h3 className='row4'>4</h3>
-          <h3 className='row4'>name4</h3>
-          <h3 className='row4'>20</h3>
-          <h3 className='row5'>5</h3>
-          <h3 className='row5'>name5</h3>
-          <h3 className='row5'>19</h3>
-          <h3 className='row6'>6</h3>
-          <h3 className='row6'>name6</h3>
-          <h3 className='row6'>16</h3>
-          <h3 className='row7'>7</h3>
-          <h3 className='row7'>name7</h3>
-          <h3 className='row7'>14</h3>
-          <h3 className='row8'>8</h3>
-          <h3 className='row8'>name8</h3>
-          <h3 className='row8'>13</h3>
-          <h3 className='row9'>9</h3>
-          <h3 className='row9'>name9</h3>
-          <h3 className='row9'>9</h3>
-          <h3 className='row10'>10</h3>
-          <h3 className='row10'>name10</h3>
-          <h3 className='row10'>1</h3>
+          <div className='rank-column'>
+            <h3 className='row1'>1</h3>
+            <h3 className='row2'>2</h3>
+            <h3 className='row3'>3</h3>
+            <h3 className='row4'>4</h3>
+            <h3 className='row5'>5</h3>
+            <h3 className='row6'>6</h3>
+            <h3 className='row7'>7</h3>
+            <h3 className='row8'>8</h3>
+            <h3 className='row9'>9</h3>
+            <h3 className='row10'>10</h3>
+          </div>
+          <div className='name-column'>
+            {highScores.map((person) => (
+              <h3 key={person.id}>{`${person.firstName} ${
+                person.lastName
+              }`}</h3>
+            ))}
+          </div>
+          <div className='score-column'>
+            {highScores.map((person) => (
+              <h3 key={person.id}>{person.score}</h3>
+            ))}
+          </div>
+          <div />
         </div>
         <h4>Login to save and view your own high scores!</h4>
         <Button buttonText={'LOGIN'} pathName={'login'} />
