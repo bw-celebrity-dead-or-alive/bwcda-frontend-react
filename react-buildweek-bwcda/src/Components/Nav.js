5977d71f9c8f520a1ec8bec4090bdaa5be5f2366
Kristin Barr <kristinbarr@gmail.com>
2019-07-31 20:12:45 -0400

    nav - added my scores

--- react-buildweek-bwcda/src/Components/Nav.js
+++ react-buildweek-bwcda/src/Components/Nav.js
@@ -4,12 +4,9 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav>
-      <Link to='/'>
-        <h3>RESET</h3>
-      </Link>
-      <Link to='/leaderboard'>
-        <h3>HALL OF FAME</h3>
-      </Link>
+      <Link to='/'>HOME</Link>
+      <Link to='/userscores'>MY SCORES</Link>
+      <Link to='/leaderboard'>HALL OF FAME</Link>
     </nav>
   )
 }
