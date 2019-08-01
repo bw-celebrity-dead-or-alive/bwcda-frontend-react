f18771fd1cbd25ba627188f58a5b302e2116ad0f
Kristin Barr <kristinbarr@gmail.com>
2019-08-01 15:11:51 -0400

    local storage to populate user scores

--- react-buildweek-bwcda/src/Components/EndingScreen.js
+++ react-buildweek-bwcda/src/Components/EndingScreen.js
@@ -6,59 +6,29 @@ const EndingScreen = () => {
   const score = window.localStorage.getItem("CorrectGuesses")
   const guess = window.localStorage.getItem("TotalGuesses")
 
-  let displayScore = score * (10 * (score / guess))
+  let displayScore = Math.floor(score * (10 * (score / guess)))
   isNaN(displayScore) ? displayScore = 0 : console.log('Is number')
 
   let isNameHere = window.localStorage.getItem("SignUpCred") || ""
 
-  window.localStorage.setItem("GameScore", JSON.stringify(Math.floor(displayScore)))
+  window.localStorage.setItem("GameScore", JSON.stringify(displayScore))
 
-  const newData () => {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
 
-    today = mm + '/' + dd + '/' + yyyy;
-    return today;
-  }
+  let today = new Date()
+  const dd = String(today.getDate()).padStart(2, '0')
+  const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  const yyyy = today.getFullYear()
+  today = mm + '/' + dd + '/' + yyyy;
 
+  const tempdate = new Date().toString().slice(4, 15) // -> returns Aug 01 2019
 
   if (isNameHere) {
-    let oldItems = JSON.parse(window.localStorage.getItem(isNameHere)) || []
-
-    let newItem = { 'score': displayScore, 'date': 'date' }
-
-    window.localStorage.setItem(isNameHere, JSON.stringify([]))
+    let oldGames = JSON.parse(window.localStorage.getItem(isNameHere)) || []
+    let newGame = { 'score': displayScore, 'date': today }
+    oldGames.push(newGame)
+    window.localStorage.setItem(isNameHere, JSON.stringify(oldGames))
   }
 
-  const tempdate = new Date().toString.slice(4, 15)
-  console.log(tempdate)
-
-  /**
-   * var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
-   * 
-   * 
-   * 
-   * var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
-  
-  var newItem = {
-      'product-name': itemContainer.find('h2.product-name a').text(),
-      'product-image': itemContainer.find('div.product-image img').attr('src'),
-      'product-price': itemContainer.find('span.product-price').text()
-  };
-  
-  oldItems.push(newItem);
-  
-  localStorage.setItem('itemsArray', JSON.stringify(oldItems));
-   * 
-   */
   return (
     <div className='end-screen'>
       <header className='end-content'>
--- react-buildweek-bwcda/src/Components/UserScores.js
+++ react-buildweek-bwcda/src/Components/UserScores.js
@@ -1,9 +1,3 @@
-/* 
- Store into local data under a UserName Key that is an array of objects, and each time a new game occurs we want to use the spread operator to 
- add another instance of the game and then store it all back into local storage to be pulled out into UserScores.
- 
- */
-
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Button from './Button'
@@ -12,7 +6,11 @@ const UserScores = () => {
   const [userGames, setUserGames] = useState([])
   const [playerId, setPlayerId] = useState(5)
 
-  useEffect(() => {
+  /*
+    Store into local data under a UserName Key that is an array of objects, and each time a new game occurs we want to use the spread operator to add another instance of the game and then store it all back into local storage to be pulled out into UserScores.
+   */
+
+  // useEffect(() => {
     //example
     //   const token = localStorage.getItem('token') || null;
     //   axios.get('url', {
@@ -21,9 +19,7 @@ const UserScores = () => {
     //     }
     //   })
     //   axios.post('url', {name: 'krishna', email: 'email@me.com'}, {
-    //     headers: {
-
-    //     }
+    //     headers: {}
     //   })
     // const Axios = axios.create({
     //   header: {
@@ -31,28 +27,35 @@ const UserScores = () => {
     //   }
     // });
     //end example
-    axios
-      .get(`https://prod-celebrity-dead-alive.herokuapp.com/api/players/${playerId}/scores`)
-      .then((res) => {
-        // console.log(res)
-        const userGamesArr = res.data
-          .sort((a, b) => {
-            return b.score - a.score
-          })
-          .slice(0, 10)
-        setUserGames(userGamesArr)
-      })
-      .catch((err) => {
-        return `Failed to load user's high scores`, err
-      })
-  }, [])
 
+  //   axios
+  //     .get(`https://prod-celebrity-dead-alive.herokuapp.com/api/players/${playerId}/scores`)
+  //     .then((res) => {
+  //       // console.log(res)
+  //       const userGamesArr = res.data
+  //         .sort((a, b) => {
+  //           return b.score - a.score
+  //         })
+  //         .slice(0, 10)
+  //       setUserGames(userGamesArr)
+  //     })
+  //     .catch((err) => {
+  //       return `Failed to load user's high scores`, err
+  //     })
+  // }, [])
+
+  let isNameHere = window.localStorage.getItem('SignUpCred') || ''
+
+  let games;
+  if (isNameHere) {
+    games = JSON.parse(window.localStorage.getItem(isNameHere))
+  }
 
   return (
     <div className='userscores-screen'>
       <header className='userscores-content'>
         <h1>HALL OF FAME</h1>
-        <h2>{userGames.map(e => e.name)[0]}</h2>
+        <h2>{isNameHere}</h2>
         <div className='grid-titles'>
           <h2>RANK</h2>
           <h2>SCORE</h2>
@@ -60,16 +63,18 @@ const UserScores = () => {
         </div>
         <div className='grid-container'>
           <div className='rank-column'>
-            {userGames.map((user, i) => <h3 key={user.id}>{i + 1}</h3>)}
+            {games.map((game, i) => (
+              <h3 key={game.score}>{i + 1}</h3>
+            ))}
           </div>
           <div className='score-column'>
-            {userGames.map((user) => (
-              <h3 key={user.id}>{user.score}</h3>
+            {games.map((game) => (
+              <h3 key={game.score}>{game.score}</h3>
             ))}
           </div>
           <div className='date-column'>
-            {userGames.map((user) => (
-              <h3 key={user.id}>{user.created_at.slice(0, 10)}</h3>
+            {games.map((game) => (
+              <h3 key={game.score}>{game['date']}</h3>
             ))}
           </div>
           <div />
